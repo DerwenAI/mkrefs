c8f4d23bc5134ea220861fe8fdbf35903e1fe81b
Paco Nathan <ceteri@gmail.com>
2021-05-11 08:30:02 -0700

    first biblio rendered

--- mkrefs/plugin.py
+++ mkrefs/plugin.py
@@ -1,13 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-
-#import sys ; sys.path.insert(0, "../kglab/")
-#import kglab
+import sys ; sys.path.insert(0, "../kglab/")
+import kglab
 
 from pprint import pprint
 import mkdocs.config
 import mkdocs.plugins
 import mkdocs.structure.files
 import pathlib
+
+import jinja2  # pylint: disable=E0401
+import json
+import tempfile
+import typing
 
 
 class MkRefsPlugin (mkdocs.plugins.BasePlugin):
@@ -21,7 +27,7 @@ class MkRefsPlugin (mkdocs.plugins.BasePlugin):
         print("__init__", self.config_scheme)
 
         self.enabled = True
-        self.graph_file = None
+        self.biblio_kg = None
         self.biblio_file = None
 
 
@@ -30,13 +36,9 @@ class MkRefsPlugin (mkdocs.plugins.BasePlugin):
         pprint(config)
 
         if self.config["biblio_graph"]:
-            self.graph_file = self.config["biblio_graph"]
-
-            biblio_path = pathlib.Path(config["docs_dir"]) / self.graph_file
-
-            #kg = kglab.KnowledgeGraph()
-            #kg.load_rdf(biblio_path)
-            #pprint(vars(kg))
+            biblio_path = pathlib.Path(config["docs_dir"]) / self.config["biblio_graph"]
+            self.biblio_kg = kglab.KnowledgeGraph()
+            self.biblio_kg.load_rdf(biblio_path, format="ttl")
 
         return config
 
@@ -47,9 +49,6 @@ class MkRefsPlugin (mkdocs.plugins.BasePlugin):
 
 
     def on_files (self, files, config, **kwargs):
-        print("on_files")
-        pprint(files)
-
         for file in files.documentation_pages():
             print(file.src_path, file.dest_path)
 
@@ -60,23 +59,21 @@ class MkRefsPlugin (mkdocs.plugins.BasePlugin):
             use_directory_urls = config["use_directory_urls"],
             )
 
-        print(self.biblio_file)
-        pprint(vars(self.biblio_file))
+        files.append(self.biblio_file)
 
+        groups = transform_to_groups(self.biblio_kg)
         biblio_path = pathlib.Path(config["docs_dir"]) / self.biblio_file.src_path
 
         with open(biblio_path, "w") as f:
-            f.write("# Bibliography\n")
-            f.write("pls read sekret stuffs\n")
-
-        files.append(self.biblio_file)
+            template = get_jinja2_template("biblio.template", config["docs_dir"])
+            f.write(template.render(groups=groups))
 
         return files
 
 
     def on_page_read_source (self, page, config, **kwargs):
         print("on_page_read_source")
-        pprint(vars(page.file))
+        #pprint(vars(page.file))
 
         if page.file.abs_src_path == config["biblio_page"]:
             print("FOUND!", config["biblio_page"])
@@ -88,9 +85,11 @@ class MkRefsPlugin (mkdocs.plugins.BasePlugin):
     ## unused events
 
     def on_env (self, env, config, files, **kwargs):
+        print("on_env")
         return env
 
     def on_nav (self, nav, config, files, **kwargs):
+        print("on_nav")
         return nav
 
     def on_pre_template (self, template, template_name, config, **kwargs):
@@ -122,3 +121,153 @@ class MkRefsPlugin (mkdocs.plugins.BasePlugin):
 
     def on_serve (self, server, **kwargs):
         return server
+
+
+######################################################################
+## bibliography
+
+def get_jinja2_template (
+    template_file: str,
+    dir: str,
+    ) -> jinja2.Template:
+    """
+Load a Jinja2 template.
+    """
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(dir),
+        autoescape=True,
+        )
+
+    return env.get_template(template_file)
+
+
+def is_kind (
+    item: dict,
+    kind_list: typing.List[str],
+    ) -> bool:
+    """
+Test whether the given JSON-LD content item has a "@type" within the
+specified list.
+    """
+    if "@type" not in item:
+        return False
+
+    return any(map(lambda k: item["@type"].endswith(k), kind_list))  # pylint: disable=W0108
+
+
+def transform_to_groups (
+    kg: kglab.KnowledgeGraph,  # pylint: disable=W0621
+    ) -> typing.Dict[str, list]:
+    """
+Transform a KG into groups of entries that can be rendered
+    """
+    # serialize as JSON-LD
+    json_path = pathlib.Path(tempfile.NamedTemporaryFile().name)
+    kg.save_jsonld(json_path)
+
+    # extract content as JSON
+    bib_g = []
+
+    with open(json_path, "r") as f:  # pylint: disable=W0621
+        bib_j = json.load(f)
+        bib_g = bib_j["@graph"]
+
+    # what are the types of content?
+    types = {  # pylint: disable=W0612
+        item["@type"]
+        for item in bib_g
+        if "@type" in item
+        }
+    #ic(types)
+
+    # who are the authors?
+    authors = {
+        item["@id"]: item
+        for item in bib_g
+        if is_kind(item, ["Author"])
+        }
+    #ic(authors)
+
+    # which are the publishers?
+    pubs = {
+        item["@id"]: item
+        for item in bib_g
+        if is_kind(item, ["Collection", "Journal", "Proceedings"])
+        }
+    #ic(pubs)
+
+    # enumerate and sort the content entries
+    content = sorted(
+        [
+            item
+            for item in bib_g
+            if is_kind(item, ["Article", "Slideshow"])
+            ],
+        key = lambda item: item["https://derwen.ai/ns/v1#citeKey"],
+        )
+    #ic(content)
+
+    # initialize the `groups` grouping of entries
+    letters = sorted(list({
+                item["https://derwen.ai/ns/v1#citeKey"][0].upper()
+                for item in content
+                }))
+
+    groups: typing.Dict[str, list] = {  # pylint: disable=W0621
+        l: []
+        for l in letters
+        }
+
+    # build the grouping of content entries, with the authors and
+    # publishers denormalized
+    for item in content:
+        #ic(item)
+
+        trans = {
+            "citekey": item["https://derwen.ai/ns/v1#citeKey"],
+            "type": item["@type"].split("/")[-1],
+            "url": item["@id"],
+            "date": item["dct:date"]["@value"],
+            "title": item["dct:title"],
+            "abstract": item["http://purl.org/ontology/bibo/abstract"],
+            }
+
+        trans["auth"] = [
+            {
+                "url": auth["@id"],
+                "name": authors[auth["@id"]]["http://xmlns.com/foaf/0.1/name"],
+                }
+            for auth in item["http://purl.org/ontology/bibo/authorList"]["@list"]
+            ]
+
+        if "http://purl.org/ontology/bibo/doi" in item:
+            trans["doi"] = item["http://purl.org/ontology/bibo/doi"]["@value"]
+
+        if "https://derwen.ai/ns/v1#openAccess" in item:
+            trans["open"] = item["https://derwen.ai/ns/v1#openAccess"]["@id"]
+
+        if "dct:isPartOf" in item:
+            pub = pubs[item["dct:isPartOf"]["@id"]]
+
+            trans["pub"] = {
+                "url": pub["dct:identifier"]["@id"],
+                "title": pub["http://purl.org/ontology/bibo/shortTitle"],
+                }
+
+            if "http://purl.org/ontology/bibo/volume" in item:
+                trans["pub"]["volume"] = item["http://purl.org/ontology/bibo/volume"]["@value"]
+
+            if "http://purl.org/ontology/bibo/issue" in item:
+                trans["pub"]["issue"] = item["http://purl.org/ontology/bibo/issue"]["@value"]
+
+            if "http://purl.org/ontology/bibo/pageStart" in item:
+                trans["pub"]["pageStart"] = item["http://purl.org/ontology/bibo/pageStart"]["@value"]
+
+            if "http://purl.org/ontology/bibo/pageEnd" in item:
+                trans["pub"]["pageEnd"] = item["http://purl.org/ontology/bibo/pageEnd"]["@value"]
+
+        #ic(trans)
+        letter = item["https://derwen.ai/ns/v1#citeKey"][0].upper()
+        groups[letter].append(trans)
+
+    return groups
