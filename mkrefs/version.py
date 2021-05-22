9bde3b91d10a62d5d78606f403562ccf457caa3b
Paco Nathan <ceteri@gmail.com>
2021-05-21 21:12:00 -0700

    needs version

---
+++ mkrefs/version.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# see license https://github.com/DerwenAI/mkrefs#license-and-copyright
+
+import sys
+import typing
+
+
+######################################################################
+## Python version checking
+
+MIN_PY_VERSION: typing.Tuple = (3, 6,)
+__version__: str = "0.1.0"
